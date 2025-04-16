dcacb6b7814ca30451e4ecf08dedb686db254ad9
r0nsha <r0nsh4v1t@gmail.com>
2025-04-16 16:10:29 +0300

    remove custom css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import logo from "./assets/ptsg_logo.svg";
-import "./App.css";
 
-function App() {
+export default function App() {
 	return (
-		<div>
+		<>
 			<img src={logo} width="200" height="200" alt="Logo" />
 			<h1>שלום לכם</h1>
 			<p>זה עוד טקסט</p>
-		</div>
+		</>
 	);
 }
-
-export default App;
