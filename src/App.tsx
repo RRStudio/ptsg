00ec279012f54726e415ec0ea66857a27bad162a
r0nsha <r0nsh4v1t@gmail.com>
2025-04-17 16:34:28 +0300

    organize

--- src/App.tsx
+++ src/App.tsx
@@ -25,49 +25,64 @@ export default function App() {
 	return (
 		<div class="w-full h-full p-8 flex justify-center">
 			<div class="flex flex-col items-center gap-12">
-				<div class="w-full flex justify-between items-center gap-8">
-					<div class="flex justify-center items-center gap-8">
-						<a href="/">
-							<img src={logo} width="240" height="240" alt="Logo" />
-						</a>
-						<Link href="/about">אודות</Link>
-						<Link href="/episodes">פרקים</Link>
-						<Link href="/ask">שאל אותנו שאלה</Link>
-						<Link href="/contact">צור קשר</Link>
-					</div>
-
-					<div class="flex gap-3">
-						<IconLink
-							icon="i-fa6-brands-telegram"
-							href="https://t.me/ptsgdev"
-						/>
-						<IconLink
-							icon="i-fa6-brands-facebook"
-							href="https://www.facebook.com/groups/ptsgdev"
-						/>
-					</div>
-				</div>
-
-				<h1 class="text-8xl font-900 text-center m-0">כל מה שלא טכני בהייטק</h1>
-
-				<p class="text-xl text-center">
-					<span class="text-primary font-bold">פותחים סוגריים</span> הוא פודקאסט
-					שבועי על כל מה שלא טכני בהייטק (ואולי גם כן).
-					<br />
-					<Link style="inline-link" href="/about">
-						אנחנו
-					</Link>{" "}
-					נענה על השאלות שלכם, וניתן את התשובות הנכונות (והלא נכונות) - כדאי לכם
-					להקשיב לנו, כי ככה.
-				</p>
-
-				<div class="flex flex-col items-center gap-3">
-					<span class="text-xl font-bold">יש לך שאלה לרון ורועי?</span>
-					<Link style="button" href="/ask">
-						שאל אותנו שאלה
-					</Link>
-				</div>
+				<TopNav />
+				<Hero />
+				<AskUs />
 			</div>
+		</div>
+	);
+}
+
+function TopNav() {
+	return (
+		<div class="w-full flex justify-between items-center gap-8">
+			<div class="flex justify-center items-center gap-8">
+				<a href="/">
+					<img src={logo} width="240" height="240" alt="Logo" />
+				</a>
+				<Link href="/about">אודות</Link>
+				<Link href="/episodes">פרקים</Link>
+				<Link href="/ask">שאל אותנו שאלה</Link>
+				<Link href="/contact">צור קשר</Link>
+			</div>
+
+			<div class="flex gap-3">
+				<IconLink icon="i-fa6-brands-telegram" href="https://t.me/ptsgdev" />
+				<IconLink
+					icon="i-fa6-brands-facebook"
+					href="https://www.facebook.com/groups/ptsgdev"
+				/>
+			</div>
+		</div>
+	);
+}
+
+function Hero() {
+	return (
+		<>
+			<h1 class="text-8xl font-900 text-center m-0">כל מה שלא טכני בהייטק</h1>
+
+			<p class="text-xl text-center">
+				<span class="text-primary font-bold">פותחים סוגריים</span> הוא פודקאסט
+				שבועי על כל מה שלא טכני בהייטק (ואולי גם כן).
+				<br />
+				<Link style="inline-link" href="/about">
+					אנחנו
+				</Link>{" "}
+				נענה על השאלות שלכם, וניתן את התשובות הנכונות (והלא נכונות) - כדאי לכם
+				להקשיב לנו, כי ככה.
+			</p>
+		</>
+	);
+}
+
+function AskUs() {
+	return (
+		<div class="flex flex-col items-center gap-3">
+			<span class="text-xl font-bold">יש לך שאלה לרון ורועי?</span>
+			<Link style="button" href="/ask">
+				שאל אותנו שאלה
+			</Link>
 		</div>
 	);
 }
