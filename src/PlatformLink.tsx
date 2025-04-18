aa1090f47030ef92613ae99be7cf1e16405178a4
Roy Shavit <wwekingwwe@gmail.com>
2025-04-18 11:01:55 +0300

    platform links

--- src/PlatformLink.tsx
+++ src/PlatformLink.tsx
@@ -1,50 +1,50 @@
 import { JSX } from "solid-js/jsx-runtime";
 
 export type PlatformLinkProps = JSX.AnchorHTMLAttributes<HTMLAnchorElement> & {
-	platform: "spotify" | "apple";
+  platform: "spotify" | "apple";
 };
 
 export default function PlatformLink({
-	platform,
-	...props
+  platform,
+  ...props
 }: PlatformLinkProps) {
-	const { icon, iconColor, label, href } = (() => {
-		switch (platform) {
-			case "spotify":
-				return {
-					icon: "i-fa6-brands-spotify",
-					iconColor: "#1ed760",
-					label: "Spotify",
-					href: "https://spotify.com",
-				};
-			case "apple":
-				return {
-					icon: "i-cib-apple-podcasts",
-					iconColor: "linear-gradient(to bottom, #d56dfb, #b150e2, #872ec4)",
-					label: "Apple",
-					href: "https://podcasts.apple.com",
-				};
-		}
-	})();
+  const { icon, iconColor, label, href } = (() => {
+    switch (platform) {
+      case "spotify":
+        return {
+          icon: "i-fa6-brands-spotify",
+          iconColor: "#1ed760",
+          label: "Spotify",
+          href: "https://open.spotify.com/show/6JNfRPkgqjJNk731PGsOAW",
+        };
+      case "apple":
+        return {
+          icon: "i-cib-apple-podcasts",
+          iconColor: "linear-gradient(to bottom, #d56dfb, #b150e2, #872ec4)",
+          label: "Apple",
+          href: "https://podcasts.apple.com/podcast/ptsgdev/id1809322756",
+        };
+    }
+  })();
 
-	return (
-		<a
-			{...props}
-			href={href}
-			class={`bg-none border-2 border-neutral-400 rounded-lg text-neutral-400 font-bold text-lg px-6 py-3 cursor-pointer hover:bg-neutral-900 hover:border-neutral-900 hover:text-neutral-0 transition-all duration-200 ${props.class}`}
-		>
-			<div class="flex items-center gap-3" dir="ltr">
-				<div
-					class={`${icon} w-12 h-12 text-transparent`}
-					style={{ background: iconColor, color: "transparent" }}
-				/>
-				<div class="flex flex-col" dir="ltr">
-					<span class="text-sm">Listen on</span>
-					<span class="text-2xl line-height-snug">
-						{label} <span class="font-normal">Podcasts</span>
-					</span>
-				</div>
-			</div>
-		</a>
-	);
+  return (
+    <a
+      {...props}
+      href={href}
+      class={`bg-none border-2 border-neutral-400 rounded-lg text-neutral-400 font-bold text-lg px-6 py-3 cursor-pointer hover:bg-neutral-900 hover:border-neutral-900 hover:text-neutral-0 transition-all duration-200 ${props.class}`}
+    >
+      <div class="flex items-center gap-3" dir="ltr">
+        <div
+          class={`${icon} w-12 h-12 text-transparent`}
+          style={{ background: iconColor, color: "transparent" }}
+        />
+        <div class="flex flex-col" dir="ltr">
+          <span class="text-sm">Listen on</span>
+          <span class="text-2xl line-height-snug">
+            {label} <span class="font-normal">Podcasts</span>
+          </span>
+        </div>
+      </div>
+    </a>
+  );
 }
